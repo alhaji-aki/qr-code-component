02b4897ee5d0c9a7bf5006346df9a74b0a7187ab
Alhaji Aki <abdulkudus2922@gmail.com>
2022-04-18 17:11:46 +0000

    added style guide config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Outfit", "sans-serif"],
+      },
+      colors: {
+        lightGray: "hsl(212, 45%, 89%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        darkBlue: "hsl(218, 44%, 22%)",
+      },
+    },
   },
   plugins: [],
 };
